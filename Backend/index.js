ed69503e7f8eb8ed13106545c6eedbeeb8be18b4
korious <innovativepotencial@gmail.com>
2024-07-11 18:01:45 +0100

    mongoose and MongoDB

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,6 @@ import { PORT } from "./config.js";
 const app = express();
 
 app.listen(PORT, () => {
-    console.log(`App is listening to port: $(PORT)`);
+    console.log(`App is listening to port: ${PORT}`);
 });
 
