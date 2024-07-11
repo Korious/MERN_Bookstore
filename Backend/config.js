fa6168f9ebea722af591994ae8eb5d57e1e3faa0
korious <innovativepotencial@gmail.com>
2024-07-11 18:03:16 +0100

    MongoDB and mongoose

--- Backend/config.js
+++ Backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+    'mongodb+srv://innovativepotencial:TMBIhPiM3LZTwSn5@mernbookstore.9v0bt4j.mongodb.net/books-collection?retryWrites=true&w=majority&appName=mernBookstore'
